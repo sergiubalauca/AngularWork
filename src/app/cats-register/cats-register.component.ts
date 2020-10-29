6ceb549ce9dd029cdaed33c4e91ef4052c112807
Balauca George-Sergiu <sooty2face@yahoo.com>
2020-10-29 23:13:46 +0200

    Added Form with validations
    Added a register cat form with a model for it and added validations using ngx

--- src/app/cats-register/cats-register.component.ts
+++ src/app/cats-register/cats-register.component.ts
@@ -7,15 +7,24 @@ import { RegisterCat } from '../register-cat';
   styleUrls: ['./cats-register.component.css']
 })
 export class CatsRegisterComponent implements OnInit {
-  
+
   /* Angular forms */
-  public colors = ['red','green','blue'];
+  public colors = ['red', 'green', 'blue'];
   /* Two way binding with [(catname)] in the template - instead of using name = "catname ngModel" */
   public catname = "";
   /* Declare a variable of type RegisterCat, in order to instantiate it with data from the template */
-  registerCat = new RegisterCat('','Green',null,'evening',true);
+  registerCat = new RegisterCat('', 'default', null, 'evening', true);
+
+  catColorHasError = true;
 
   constructor() { }
+
+  validateCatColor(value) {
+    if (value === 'default')
+      this.catColorHasError = true;
+    else
+      this.catColorHasError = false;
+  }
 
   ngOnInit(): void {
   }
