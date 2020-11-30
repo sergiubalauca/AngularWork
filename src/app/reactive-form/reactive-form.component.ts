ceab9531875e917ac40caa138335dee197275281
Balauca George-Sergiu <sooty2face@yahoo.com>
2020-11-30 14:49:37 +0200

    Added conditional validation
    Added the email input in the form, along with a 'subscribe' checkbox, making it so that the email is required when the subscribe checkbox is checked. For that, I used the valueChanges property and the setValidators method.

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -22,7 +22,7 @@ export class ReactiveFormComponent implements OnInit {
   private addressID: number;
   private subscription: Subscription;
   private subscriptionAddresses: Subscription;
-
+  registrationForm: FormGroup;
 
   constructor(private employeeService: EmployeeService,
     private addressService: AddressService,
@@ -33,6 +33,10 @@ export class ReactiveFormComponent implements OnInit {
   /* A getter for retrieving the name value, in order to use it in the template */
   get studentName() {
     return this.registrationForm.get('name');
+  }
+
+  get email() {
+    return this.registrationForm.get('email');
   }
 
   /* We initialize a new FormGroup object with the FormControls from the html as input in the constructor */
@@ -48,22 +52,26 @@ export class ReactiveFormComponent implements OnInit {
     })
   });
 
-  /* 2nd way of doing it by using an injected service of type FormBuilder, so no need for new instances of FormControl/Group */
-  registrationForm = this.fb.group({
-    /* Applied the required validators rule for this field as second argument.
-     * For multiple validations applied, we use an array
-     */
-    name: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator2(/password/)]],
-    password: [''],
-    confirmPassword: [''],
-    address: this.fb.group({
-      city: [''],
-      state: [''],
-      postalCode: ['']
-    })
-  },{validators: PasswordValidator}); /* I added the validator the the entire group, so that we can include both pass and confirm pass, not the separate controls*/
+
 
   ngOnInit(): void {
+    /* 2nd way of doing it by using an injected service of type FormBuilder, so no need for new instances of FormControl/Group */
+    this.registrationForm = this.fb.group({
+      /* Applied the required validators rule for this field as second argument.
+       * For multiple validations applied, we use an array
+       */
+      name: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator2(/password/)]],
+      email: [''],
+      password: [''],
+      confirmPassword: [''],
+      subscribe: [false],
+      address: this.fb.group({
+        city: [''],
+        state: [''],
+        postalCode: ['']
+      })
+    }, { validators: PasswordValidator }); /* I added the validator the the entire group, so that we can include both pass and confirm pass, not the separate controls*/
+
     this.id = parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
     this.addressID = parseInt(this.activatedRoute.snapshot.paramMap.get('adrsID'));
     //console.log(this.id);
@@ -75,7 +83,8 @@ export class ReactiveFormComponent implements OnInit {
            * To assign them all, we use setValue() 
            */
           this.registrationForm.patchValue({
-            name: data.name
+            name: data.name,
+            email: data.email
           })
 
           this.subscriptionAddresses = this.addressService.getAddress(data.addressID)
@@ -91,21 +100,22 @@ export class ReactiveFormComponent implements OnInit {
             })
         })
     }
-  }
 
-  loadApi() {
-    this.id = parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
-    //console.log(this.id);
-    if (!isNaN(this.id)) {
-      this.subscription = this.employeeService.getEmployee(this.id)
-        .subscribe(data => {
-          console.log(data.name)
-          /* Update the binded object to the template */
-          this.registrationForm.patchValue({
-            name: data.name
-          })
-        });
-    }
+    /* For conditional validation - we make use of the valueChanges property which returns an Observable.
+     * We subscribe to it and we get the check value as a parameter 
+     */
+    this.registrationForm.get('subscribe').valueChanges.subscribe(checkedValue => {
+      /* We get a hold of the email form control */
+      const email = this.registrationForm.get('email');
+      if (checkedValue) {
+        /* If the value is checked, we need to se the required validator */
+        email.setValidators(Validators.required);
+      }
+      else
+        email.clearValidators();
+      /* In order to ensure the correct status is reflected, we call the updateValueAndValidity method */
+      email.updateValueAndValidity();
+    })
   }
 
   ngOnDestroy() {
